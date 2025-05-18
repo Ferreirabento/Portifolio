bce8995fbe9a2c48ca53dc64df5b150e0d351d4e
Ferreirabento <jf6808932@gmai.com>
2025-05-18 15:14:43 -0300

    iniciando um projeto voltado para analisar e classificar áreas de risco ambiental em uma simulação 2D baseada em dados gerados aleatoriamente

---
+++ Análise e Classificação Espacial de Riscos Ambientais/trabalho.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#Criar um plano 2D com pontos representando diferentes posições em um mapa.
+
+
+#Calcular a “distância ao centro” de cada ponto.
+
+
+#Classificar essas distâncias em pelo menos três categorias distintas.
+
+
+#Aplicar operações vetoriais para tornar o código mais eficiente e escalável.
+
+
+#Utilizar funções universais, operadores condicionais e métodos estatísticos para análise.
+
+
+#Gerar uma visualização gráfica (como um mapa de calor ou gráfico de contorno) para mostrar os dados classificados.
+
