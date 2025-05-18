e0b12eb454ad200a130509ff96fd7d22ca961f14
Ferreirabento <jf6808932@gmai.com>
2025-05-18 17:00:48 -0300

    projeto feito com uma parte do que estudei em numpy até agora
    feito para captar a distancia da radiação emitida longe do centro

--- Análise e Classificação Espacial de Riscos Ambientais/trabalho.py
+++ Análise e Classificação Espacial de Riscos Ambientais/trabalho.py
@@ -1,19 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Criar um plano 2D com pontos representando diferentes posições em um mapa.
-
+x = np.linspace(-10, 40)
+y = np.linspace(40, -10)
+xz, yz = np.meshgrid(x, y)
 
 #Calcular a “distância ao centro” de cada ponto.
-
+z = np.sqrt(xz ** 2 + yz ** 2)
 
 #Classificar essas distâncias em pelo menos três categorias distintas.
-
-
-#Aplicar operações vetoriais para tornar o código mais eficiente e escalável.
-
-
-#Utilizar funções universais, operadores condicionais e métodos estatísticos para análise.
-
+distancia = np.where(z > 1.2 * np.mean(z),
+                      "zona crítica/perigosa",
+                        np.where(z < 0.8 * np.mean(z),
+                        "zona segura",
+                         "zona de atenção"))
 
 #Gerar uma visualização gráfica (como um mapa de calor ou gráfico de contorno) para mostrar os dados classificados.
-
+plt.title("Classificação Espacial de Riscos Ambientais")
+plt.imshow(z)
+plt.colorbar()
+plt.show()
